4a16dd34c238bbc3c82d5ae464e45db24c40eb3e
Kelley <kelleyshann@gmail.com>
2019-12-08 18:53:45 -0500

    fix: fixed app function

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,7 +5,7 @@ from . import config
 
 
 def create_app():
-    flask_app == Flask(__name__)
+    flask_app = Flask(__name__)
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     flask_app.app_context().push()
