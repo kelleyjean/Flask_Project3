e0f74ea7a9cd671bd031145cc7d0ed8d3dcceada
Kelley Shann <kelleyshann@gmail.com>
2019-12-14 12:02:20 -0500

    task: added two more functions into blogpostview

--- blog_api/src/views/Blogpostview.py
+++ blog_api/src/views/Blogpostview.py
@@ -1,4 +1,4 @@
-#/src/views/BlogpostView.py
+# /src/views/BlogpostView.py
 from flask import request, g, Blueprint, json, Response
 from ..shared.Authentication import Auth
 from ..models.BlogpostModel import BlogpostModel, BlogpostSchema
@@ -10,26 +10,48 @@ blogpost_schema = BlogpostSchema()
 @blogpost_api.route('/', methods=['POST'])
 @Auth.auth_required
 def create():
-  """
+    """
   Create Blogpost Function
   """
-  req_data = request.get_json()
-  req_data['owner_id'] = g.user.get('id')
-  data, error = blogpost_schema.load(req_data)
-  if error:
-    return custom_response(error, 400)
-  post = BlogpostModel(data)
-  post.save()
-  data = blogpost_schema.dump(post).data
-  return custom_response(data, 201)
+    req_data = request.get_json()
+    req_data['owner_id'] = g.user.get('id')
+    data, error = blogpost_schema.load(req_data)
+    if error:
+        return custom_response(error, 400)
+    post = BlogpostModel(data)
+    post.save()
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 201)
+
+
+@blogpost_api.route('/', methods=['GET'])
+def get_all():
+    """
+  Get all blog posts
+  """
+    posts = BlogpostModel.get_all_blogposts()
+    data = blogpost_schema.dump(posts, many=True).data
+    return custom_response(data, 200)
+
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['GET'])
+def get_one(blogpost_id):
+    """
+  Get a blog post
+  """
+    post = BlogpostModel.get_one_blogpost(blogpost_id)
+    if not post:
+      return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 200)
 
 
 def custom_response(res, status_code):
-  """
+    """
   Custom Response Function
   """
-  return Response(
-    mimetype="application/json",
-    response=json.dumps(res),
-    status=status_code
-  )
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
